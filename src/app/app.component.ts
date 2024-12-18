959ca3c54dd9edcb3ee526365bfd105f3aa657ef
tushar jadhav <tushar7.jadhav@ril.com>
2024-12-18 12:09:12 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,202 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'checkpoint-widget';
+  @ViewChild("inner", { static: false }) inner: ElementRef;
+  @ViewChild("route", { static: false }) route: ElementRef;
+  selectedFile: File | null = null;
+  imagePreview: string | null = null;
+  checkpointGroup: UntypedFormGroup[] = []
+  checkpoint: UntypedFormGroup
+  element
+  private isDragging = false;
+  private currentDraggingIndex = -1;
+  private offsetX = 0;
+  private offsetY = 0;
+  fix_height: number = 70
+  parentHeight = 600;
+  parentWidth = 500;
+  backgroundurl;
+  routeurl;
+  payload;
+  isPreviewOpen:boolean=false
+
+
+  constructor(private formdata: UntypedFormBuilder, private cdr: ChangeDetectorRef) { }
+  ngOnInit() { 
+    this.handleAddMore()
+  }
+  handleCheckpointElement(i){
+    console.log(i)
+  }
+  handleSizeChange(event, i: any) {
+    const currentFormGroup = this.checkpointGroup[i];
+    const heights = this.route.nativeElement.offsetHeight;
+    currentFormGroup.patchValue({
+      height: event.target.value,
+      heightPerc:(event.target.value / heights) * 100
+
+    })
+    this.cdr.detectChanges();
+  }
+
+  addInitialCheckpoint() {
+    const data = this.formdata.group({
+      image: [''],
+      top: [0],
+      left: [0],
+      topPerc: [0],
+      leftPerc: [0],
+      isDragging: [false]
+    });
+    this.checkpointGroup.push(data);
+  }
+  handleDelete(i: number) {
+    this.checkpointGroup.splice(i, 1)
+
+  }
+  handleHeight(e: Event) {
+    const target = e.target as HTMLInputElement;
+    this.parentHeight = Number(target.value);
+  }
+
+  handleWidth(e: Event) {
+    const target = e.target as HTMLInputElement;
+    this.parentWidth = Number(target.value);
+  }
+  handleAddMore() {
+    const form = this.formdata.group({
+      image: [''],
+      top: [0],
+      left: [0],
+      height: [70],
+      width: [0],
+      topPerc: [0],
+      leftPerc: [0],
+      isDragging: [false],
+      heightPerc:[this.fix_height]
+    });
+    this.checkpointGroup.push(form);
+  }
+
+  onMouseDown(event: MouseEvent, index: number): void {
+    this.isDragging = true;
+    this.currentDraggingIndex = index;
+
+    const currentFormGroup = this.checkpointGroup[index];
+    const left = currentFormGroup.get('left').value;
+    const top = currentFormGroup.get('top').value;
+
+    this.offsetX = event.clientX - left;
+    this.offsetY = event.clientY - top;
+
+    document.body.style.userSelect = 'none';
+  }
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent): void {
+    if (this.isDragging && this.currentDraggingIndex !== -1) {
+      const newLeft = event.clientX - this.offsetX;
+      const newTop = event.clientY - this.offsetY;
+      const height = this.route.nativeElement.offsetHeight;
+      const width = this.route.nativeElement.offsetWidth;
+
+      const currentFormGroup = this.checkpointGroup[this.currentDraggingIndex];
+      currentFormGroup.patchValue({
+        left: newLeft,
+        top: newTop,
+        topPerc: (newTop / height) * 100,
+        leftPerc: (newLeft / width) * 100,
+      });
+
+      this.cdr.detectChanges();
+    }
+  }
+
+  @HostListener('document:mouseup')
+  onMouseUp(): void {
+    this.isDragging = false;
+    this.currentDraggingIndex = -1;
+    document.body.style.userSelect = 'auto';
+  }
+  handleRouteChange(event: any): void {
+    const file = event.target.files[0];
+    if (file) {
+      console.log("Selected file:", file);
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        const backgroundImageUrl = e.target.result;
+        this.routeurl = backgroundImageUrl
+        this.route.nativeElement.style.backgroundImage = `url(${this.routeurl})`;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  handleFileChange(event: any): void {
+    const file = event.target.files[0];
+    if (file) {
+      console.log("Selected file:", file);
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        const backgroundImageUrl = e.target.result;
+        this.backgroundurl = backgroundImageUrl
+        this.inner.nativeElement.style.backgroundImage = `url(${this.backgroundurl})`;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+  
+  handleSubmit() {
+    this.isPreviewOpen=!this.isPreviewOpen
+    let checkpointdata = [];
+    for (let i = 0; i < this.checkpointGroup.length; i++) {
+      const { 
+        height:native,
+        image: checkpointImage, 
+        topPerc: checkpointTopPercentage, 
+        leftPerc: checkpointLeftPercentage,
+        } = this.checkpointGroup[i].value;
+        const heights = this.route.nativeElement.offsetHeight;
+        let checkpointHeightPercentage=(native/ heights) * 100
+      checkpointdata.push({  checkpointImage, checkpointTopPercentage, checkpointLeftPercentage ,checkpointHeightPercentage});
+    }
+    this.payload = {
+      aspectRatio: `${this.parentHeight}/${this.parentWidth}`,
+      background_image: this.backgroundurl,
+      route: this.routeurl,
+      checkpoints: checkpointdata
+    }
+
+  }
+
+  handlebackgroundImage(event:any,i){
+    const target = event.target as HTMLInputElement;
+    
+    const file = target.files[0];
+    if (file) {
+      console.log("Selected file:", file);
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        const backgroundImageUrl = e.target.result;
+
+        this.checkpointGroup[i].patchValue({image:backgroundImageUrl})
+        
+      };
+      reader.readAsDataURL(file);
+    }
+
+    this.cdr.detectChanges();
+  }
+
 }
